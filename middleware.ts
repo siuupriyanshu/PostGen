ccdcb0faeb71b8039d2655b5a09c192d6ad1b4e9
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-10-15 19:48:53 +0545

    navbar

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+ // const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect()
-  }
-})
+ /* export default clerkMiddleware((auth, request) => {
+   if (!isPublicRoute(request)) {
+     auth().protect()
+   }
+ }) */
 
-export const config = {
+   export default clerkMiddleware();
+ 
+ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
