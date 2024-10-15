c982f1cca7c720e2831714643a5857f95bea89d3
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-10-15 12:04:37 +0545

    database Schemas

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,4 +1,10 @@
 export default {
     dialect: 'postgresql',
-    schema: './utils/db/schema.ts'
-}
+    schema: './utils/db/schema.ts',
+    out: "./drizzle",
+
+    dbCredentials: {
+        url: "postgresql://cloudinary_owner:YunAsVfv2Oz0@ep-crimson-hill-a1660sro.ap-southeast-1.aws.neon.tech/PostGen?sslmode=require",
+        connectionString: "postgresql://cloudinary_owner:YunAsVfv2Oz0@ep-crimson-hill-a1660sro.ap-southeast-1.aws.neon.tech/PostGen?sslmode=require",
+    },
+};
